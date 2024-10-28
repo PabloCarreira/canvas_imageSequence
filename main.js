0294a1d3b4b24e83a87a5f023cae044fe3e7e636
Pablo Carreira <pablog.carreira@gmail.com>
2024-10-28 11:43:56 -0300

    moved imagesequence logic outside main

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { imageSequence } from './sequence.js'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -25,47 +26,16 @@ document.querySelector('#app').innerHTML = `
 `
 let frameCount = 147,
 urls = new Array(frameCount).fill().map((o, i) => `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${(i+1).toString().padStart(4, '0')}.jpg`);
-
+console.log(urls)
 gsap.registerPlugin(ScrollTrigger);
 
 setupCounter(document.querySelector('#counter'))
-function imageSequence(config) {
-  let playhead = {frame: 0},
-      canvas = gsap.utils.toArray(config.canvas)[0] || console.warn("canvas not defined"),
-      ctx = canvas.getContext("2d"),
-      curFrame = -1,
-      onUpdate = config.onUpdate,
-      images,
-      updateImage = function() {
-        let frame = Math.round(playhead.frame);
-        if (frame !== curFrame) { // only draw if necessary
-          config.clear && ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(images[Math.round(playhead.frame)], 0, 0);
-          curFrame = frame;
-          onUpdate && onUpdate.call(this, frame, images[frame]);
-        }
-      };
-  images = config.urls.map((url, i) => {
-    let img = new Image();
-    img.src = url;
-    i || (img.onload = updateImage);
-    return img;
-  });
-  return gsap.to(playhead, {
-    frame: images.length - 1,
-    ease: "none",
-    onUpdate: updateImage,
-    duration: images.length / (config.fps || 30),
-    paused: !!config.paused,
-    scrollTrigger: config.scrollTrigger
-  });
-}
 
 imageSequence({
   urls, // Array of image URLs
   canvas: document.querySelector('#image-sequence'), // <canvas> object to draw images to
   //clear: true, // only necessary if your images contain transparency
-  //onUpdate: (index, image) => console.log("drew image index", index, ", image:", image),
+  // onUpdate: (index, image) => console.log("drew image index", index, ", image:", image),
   scrollTrigger: {
     start: 0,   // start at the very top
     end: "max", // entire page
